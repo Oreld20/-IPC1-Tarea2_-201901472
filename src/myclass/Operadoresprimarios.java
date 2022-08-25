31a709740f9b49f366ee75e01f7560a3bccd6cb3
Oreld20 <3032209610108@ingenieria.usac.edu.gt>
2022-08-25 15:49:13 -0500

    first commit

---
+++ src/myclass/Operadoresprimarios.java
@@ -0,0 +1,60 @@
+
+package myclass;
+import myclass.Login2;
+public class Operadoresprimarios {
+    
+
+   public static void main(String[] args) {
+Login2 pantalla = new Login2 ();
+    pantalla.setVisible(true);
+    pantalla.setLocationRelativeTo(null);
+       
+            }}
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+           
+           
+               
+               
+               
+               
+            
+
+
+
+
+
+
+
+
+
+           
+
+
+
+   
+            
